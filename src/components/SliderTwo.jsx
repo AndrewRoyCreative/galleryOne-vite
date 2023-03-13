d884601279c18cc8a1a4771ddeffcd8bf545d138
AndrewRoyCreative <110363455+AndrewRoyCreative@users.noreply.github.com>
2023-03-13 16:32:03 -0400

    fine tuning responsiveness

--- src/components/SliderTwo.jsx
+++ src/components/SliderTwo.jsx
@@ -1,61 +1,70 @@
-import { useState } from 'react';
-import { RxDotFilled } from 'react-icons/rx';
+import React from 'react'
 
+import Artist1 from '../../assets/artist-1.png'
+import Artist2 from '../../assets/artist-2.png'
+import Artist3 from '../../assets/artist-3.png'
 
-
-function SliderTwo() {
-  const slides = [
-    {
-      title: 'My Culture',
-      url: '/assets/artist-1.png',
-    },
-    {
-      url: '/assets/artist-2.png',
-    },
-    {
-      url: '/assets/artist-3.png',
-    },
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
-
+const SliderTwo = () => {
   return (
-    <div className=' md:hidden h-[780px] w-full m-auto py-16 px-4 relative'>
-      <div
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full bg-center bg-cover duration-500'
-      ></div>
-      <div className='flex top-4 justify-center py-2'>
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
-          >
-            <RxDotFilled />
-          </div>
-        ))}
-      </div>
+    <section className='w-full h-full flex flex-col pr-10 pl-10 bg-teal-500'>
+                <div className='w-full h-full flex flex-row justify-start md:justify-between'>
+                    <div className='lg:w-[35%]'>
+                        <img 
+                            src={Artist1}
+                            alt="Artist 1"
+                        />
+                        <h5 className='text-[20px] lg:text-[20px] font-semibold mt-5'>MY CULTURE</h5>
+                            <p className='w-full mt-5 text-wrap text-[#646464] text-[14px] lg:text-[14px] font-light pr-5'
+                                >
+                                Joseph is an 23-year-old Native American who has overcome 
+                                immense adversity in his life. He has faced poverty,
+                                racism, and a lack of educational opportunities. Despite these 
+                                difficulties, Joseph has found a way to use his art to express his 
+                                courage and resilience.
+                            </p>
+                    </div>
+                    <div className='lg:w-[35%] mr-8 ml-8 hidden sm:block'>
+                        <img
+                            src={Artist2}
+                            alt="Artist 2"
+                        />
+                            <h5 className='text-[20px] font-semibold mt-5'>THE WAY FORWARD</h5>
+                                <p className='mt-5 text-wrap text-[#646464] text-[14px] lg:text-[14px] font-light pr-5'
+                                >
+                                John is an 80-year-old Native American man who uses his art to keep his culture and 
+                                heritage alive. John is determined to ensure that his culture and heritage continue 
+                                to thrive.
+                            </p>
+                    </div>
+                    <div className='w-[35%] hidden lg:block'>
+                        <img
+                            src={Artist3}
+                            alt="Artist 3"
+                        />
+                            <h5 className='text-[20px] font-semibold mt-5'>KNOWNG YOUR ROOTS</h5>
+                                <p className='mt-5 text-wrap text-[#646464] text-[14px] lg:text-[14px] font-light pr-5'
+                                    >
+                                    Marina is a 32-year-old Native South American woman who
+                                    uses her art to empower women and promote positive change. Marina has faced poverty, 
+                                    sexism, and discrimination in her life, and she has used her art to express her resilience 
+                                    and strength.
+                                </p>
+                            </div>
+                    </div>
 
-    </div>
-  );
+            <div className=' bg-red-500 w-full h-full flex flex-row justify-between '>
+                <div className='w-[35%]'>
+                    <button className=' bg-[#F3B52A] w-[200px] text-white p-1 pt-2 pb-2 mt-14 hover:bg-[#DCA52A] transition duration-700 ease-in-out'>READ MORE</button>
+                    </div>
+                    <div className='w-[35%]'>
+                    <button className=' bg-[#F3B52A] w-[200px] text-white p-1 pt-2 pb-2 mt-14 hover:bg-[#DCA52A] transition duration-700 ease-in-out'>READ MORE</button>
+                    </div>
+                    <div className='w-[35%] hidden lg:block'>
+                    <button className=' bg-[#F3B52A] w-[200px] text-white p-1 pt-2 pb-2 mt-14 hover:bg-[#DCA52A] transition duration-700 ease-in-out'>READ MORE</button>
+                    </div>
+                </div>
+</section>
+  )
 }
 
-export default SliderTwo;
-
+export default SliderTwo
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+
 
 import Hero from '../../assets/hero-img.png'
 import WhatsOn from '../../assets/whats-on-img.png'
 
-import Slider from '../components/Slider'
-
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import SliderTwo from '../components/SliderTwo'
+import Slider from '../components/Slider'
 
-import { motion } from "framer-motion"
 
 
 const Home = () => {
@@ -19,46 +17,27 @@ const Home = () => {
     <Navbar />
 
     {/* HERO HEADING */}
-    <section className='h-[50vh] sm:h-[70vh] w-full flex flex-col justify-end pl-10 pr-10'>
+    <section className='h-[50vh] sm:h-[60vh] w-full flex flex-col justify-end pl-10 pr-10'>
             <div className='flex-col flex lg:flex-row justify-between pb-3 lg:items-end '>
                 <div>
                     <div className='overflow-hidden'>
-                    <motion.h1 
-                        className='text-[76px] sm:text-[92px] text-black leading-none font-semibold'
-                        initial={{y:100, opacity:1}}
-                        animate={{y:0, opacity:1}}
-                        transition={{delay: 0.5, duration: 0.9, ease: [0.56, 0.02, 0.37, 1.1]}}
-                        >
-                        THIS
-                        </motion.h1>
+                    <h1 className='text-[72px] sm:text-[82px] text-black leading-none font-semibold'>THIS</h1>
                     </div>
                         <div className='flex flex-row items-center overflow-hidden'>
-                        <motion.div 
-                            className='w-[80px] h-[60px] border-[10px] sm:w-[130px] lg:w-full sm:h-[80px] sm:border-[16px] border-primary'
-                                initial={{y:-100, opacity:1}}
-                                animate={{y:0, opacity:1}}
-                                transition={{delay: 1.0, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                            />
-                            <motion.h1 
-                                className='text-[72px] sm:text-[92px] text-black leading-none font-semibold'
-                                initial={{y:100, opacity:1}}
-                                animate={{y:0, opacity:1}}
-                                transition={{delay: 0.7, duration: 0.9, ease: [0.56, 0.02, 0.37, 1.1]}}
-                                >IS</motion.h1>
+                        <div 
+                            className='w-[80px] h-[60px] border-[10px] sm:w-[130px] lg:w-full sm:h-[80px] sm:border-[16px] border-primary'/>
+                            <h1 className='text-[72px] sm:text-[82px] text-black leading-none font-semibold'>IS</h1>
                         </div>
-                </div>
+                    </div>
                 <div className=' hidden w-full lg:w-[40%] h-full lg:flex flex-col justify-end text-wrap pb-3 pt-3'>
-                    <motion.p 
-                        className='text-[12px] sm:text-[14px] font-light'
-                            initial={{opacity:0}}
-                            animate={{opacity:1}}
-                            transition={{delay: 1.0, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                                >You won't want to miss this one-of-a-kind exhibition of Native American art!
-                                The show will feature the work of some of the most talented Native American artists,
-                                who will be showcasing their unique perspectives and cultural heritage.
-                                This is a great opportunity to appreciate the beauty and power of Native American 
-                                art and to support the work of these amazing artists. Don't miss out on this incredible event!
-                    </motion.p>
+                    <p className='text-[12px] sm:text-[14px] font-light'
+                        >
+                        You won't want to miss this one-of-a-kind exhibition of Native American art!
+                        The show will feature the work of some of the most talented Native American artists,
+                        who will be showcasing their unique perspectives and cultural heritage.
+                        This is a great opportunity to appreciate the beauty and power of Native American 
+                        art and to support the work of these amazing artists. Don't miss out on this incredible event!
+                    </p>
                 </div>
             </div>
     </section>
@@ -67,81 +46,47 @@ const Home = () => {
     {/* HERO IMAGE AND BUTTON */}
         <section>
                 <div className='pr-10 pl-10'>
-                    <motion.img 
+                    <img 
                         className='w-[100%] h-[100%]'
                         src={Hero}
                         alt='Modern Painting'
-                        initial={{opacity:0}}
-                        animate={{ opacity:1}}
-                        transition={{delay: 1.7, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
                     />
                 </div>
                 <div className=' w-full h-full flex flex-col justify-end text-wrap pt-3 pr-10 pl-10 lg:hidden mt-5'>
-                    <motion.p 
-                        className='text-[14px] font-light'
-                        initial={{opacity:0}}
-                        animate={{opacity:1}}
-                        transition={{delay: 1.0, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
+                    <p className='text-[14px] font-light'
                         >
                         You won't want to miss this one-of-a-kind exhibition of Native American art!
                         The show will feature the work of some of the most talented Native American artists,
                         who will be showcasing their unique perspectives and cultural heritage.
                         This is a great opportunity to appreciate the beauty and power of Native American 
                         art and to support the work of these amazing artists. Don't miss out on this incredible event!
-                    </motion.p>
+                    </p>
                 </div>
         </section>
-        <motion.div 
-            className='w-full h-[200px] flex items-center justify-center'
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ delay:0.2, duration: 0.9, ease: [0.56, 0.02, 0.37, 1.1]}}
-            >
+        <div className='w-full h-[200px] flex items-center justify-center'>
             <button 
                 className='bg-primary pt-3 pb-3 pr-8 pl-8 text-white text-sm font-bold hover:bg-[#14A3AC] transition duration-700 ease-in-out'
-                >VIEW COLLECTION</button>
-        </motion.div>
+                >
+                VIEW COLLECTION
+            </button>
+        </div>
 
 
     {/* EXHIBIT LIST HEADING */}
     <section className=' w-full flex flex-col justify-end pl-10 pr-10 mt-20'>
             <div className='flex flex-col lg:flex-row justify-between items-start lg:items-center'>
                 <div>
-                    <div className='overflow-hidden'>
-                    <motion.h1 
-                        className='text-[72px] sm:text-[92px] text-black leading-none font-semibold'
-                        initial={{y:100, opacity:1}}
-                        animate={{y:0, opacity:1}}
-                        transition={{delay: 0.5, duration: 0.9, ease: [0.56, 0.02, 0.37, 1.1]}}
-                        >
-                        WHATS
-                        </motion.h1>
-                        </div>
+                    <h1 className='text-[70px] sm:text-[82px] text-black leading-none font-semibold'>WHATS</h1>
                         <div className='flex flex-row items-center overflow-hidden'>
-                        <motion.div 
-                            className='w-full h-[80px] border-[16px] border-[#EA1B2F]'
-                            initial={{y:-100, opacity:1}}
-                            animate={{y:0, opacity:1}}
-                            transition={{delay: 1.0, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                            />
-                            <motion.h1 
-                                className='text-[72px] sm:text-[92px] text-black leading-none font-semibold'
-                                initial={{y:100, opacity:1}}
-                                animate={{y:0, opacity:1}}
-                                transition={{delay: 0.7, duration: 0.9, ease: [0.56, 0.02, 0.37, 1.1]}}
-                            >
-                            ON
-                            </motion.h1>
-                    </div>
-                </div>
-                <div className='w-full lg:w-[50%] h-full pt-5'>
-                <motion.img 
+                            <div className='w-full h-[80px] border-[16px] border-[#EA1B2F]'/>
+                                <h1 className='text-[70px] sm:text-[82px] text-black leading-none font-semibold'>ON</h1>
+                            </div>
+                        </div>
+            <div className='w-full lg:w-[50%] h-full pt-5'>
+                <img 
                     className='w-full h-full lg:pr-10 lg:pl-10'
                     src={WhatsOn}
                     alt='Modern Painting'
-                    initial={{opacity:0}}
-                    animate={{ opacity:1}}
-                    transition={{delay: 1.7, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
                 />
                 </div>
             </div>
@@ -177,62 +122,25 @@ const Home = () => {
 
     {/* ARTICLES HEADING */}
     <section>
-        <div className=' w-full flex flex-col justify-center items-center mt-20'>
-            <div className='w-full flex flex-col lg:flex-row justify-center items-center '>
+        <div className=' w-full flex flex-col justify-center items-center mt-20 pl-10 pr-10'>
+            <div className='w-full flex flex-col lg:flex-row justify-center items-center'>
                 <div className='w-full flex flex-row justify-between lg:hidden pb-20 pr-10 pl-10'>
-                    <motion.div 
-                        className='w-[100px] h-[60px] md:w-[180px] md:h-[100px] lg:w-full lg:ml-10 lg:h-[180px] lg:hidden border-[12px] md:border-[20px] border-[#F3B52A]'
-                        initial={{x:-100, opacity:1}}
-                        animate={{x:0, opacity:1}}
-                        transition={{delay: 1.0, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                        />
-                    <motion.div 
-                        className='w-[100px] h-[60px] md:w-[180px] md:h-[100px] lg:w-full lg:ml-10 lg:h-[180px] lg:hidden border-[12px] md:border-[20px] border-[#F3B52A]'
-                        initial={{x:-100, opacity:1}}
-                        animate={{x:0, opacity:1}}
-                        transition={{delay: 1.0, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                        />
+                    <div className='w-[100px] h-[60px] md:w-[180px] md:h-[100px] lg:w-full lg:ml-10 lg:h-[180px] lg:hidden border-[12px] md:border-[20px] border-[#F3B52A]'/>
+                        <div className='w-[100px] h-[60px] md:w-[180px] md:h-[100px] lg:w-full lg:ml-10 lg:h-[180px] lg:hidden border-[12px] md:border-[20px] border-[#F3B52A]'/>
                     </div>
-                    <div className='w-full pl-10 overflow-hidden'> 
-                    <motion.div 
-                        className='hidden lg:flex lg:w-full h-[180px] border-[20px] border-[#F3B52A]'
-                        initial={{x:500, opacity:1}}
-                        animate={{x:0, opacity:1}}
-                        transition={{delay: 0.8, duration: 2.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                        />
-                        </div>
-                            <div className='flex flex-col items-center'>
-                                <div className='overflow-hidden'>
-                                <motion.h1 
-                                    className='text-center text-[72px] sm:text-[92px] text-black leading-none font-semibold mr-10 ml-10'
-                                    initial={{y:100, opacity:1}}
-                                    animate={{y:0, opacity:1}}
-                                    transition={{delay: 0.5, duration: 0.9, ease: [0.56, 0.02, 0.37, 1.1]}}
-                                    >
-                                    A FEW WORDS
-                                    </motion.h1>
-                                    </div>
-                                    <motion.p 
-                                        className=' text-[14px] w-full mt-5 pl-10 pr-10 text-center md:w-[750px] text-wrap font-light'
-                                        initial={{opacity:0}}
-                                        animate={{opacity:1}}
-                                        transition={{delay: 1.0, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
+                    <div className='hidden lg:flex lg:w-full h-[180px] border-[20px] border-[#F3B52A]'/>
+                        <div className='flex flex-col items-center'>
+                                <h1 className='text-center text-[72px] sm:text-[82px] text-black leading-none font-semibold mr-10 ml-10'>A FEW WORDS</h1>
+                                    <p className=' text-[14px] w-full mt-5 pl-10 pr-10 text-center md:w-[750px] text-wrap font-light'
                                         >
                                         Supporting Native American artists
                                         is an important way to recognize the unique cultural heritage 
                                         of Native American people. We can help ensure that their art and culture is celebrated and appreciated.
                                         It is also an opportunity to learn about different cultures and perspectives and to show our appreciation
                                         for the work of these amazing artists. Click on the articles to learn more!
-                                    </motion.p>
-                    </div>  
-                        <div className='w-full pr-10 overflow-hidden'> 
-                            <motion.div 
-                                className='hidden lg:flex lg:w-full lg:mr-10 h-[180px] border-[20px] border-[#F3B52A]'
-                                initial={{x:-500, opacity:1}}
-                                animate={{x:0, opacity:1}}
-                                transition={{delay: 0.8, duration: 2.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                                />
-                        </div>   
+                                    </p>
+                            </div>  
+                        <div className='hidden lg:flex lg:w-full h-[180px] border-[20px] border-[#F3B52A]'/>
                 </div>
          </div>
     </section>
@@ -242,49 +150,29 @@ const Home = () => {
     <Slider />
     {/* <SliderTwo /> */}
 
-
-
     {/* SECTION 5 // NEWSLETTER */}
     <section>
         <div className=' flex flex-col justify-start items-start md:justify-center md:items-center mt-40 lg:mt-64'>
             <div className='flex flex-row justify-center items-center overflow-hidden'>
-            <motion.h1 
-                className=' w-full lg:w-[70%] text-[72px] sm:text-[92px] text-black leading-none font-semibold pl-10 pr-10 lg:mr-20 lg:ml-20 text-left md:text-center'
-                initial={{opacity:0}}
-                animate={{opacity:1}}
-                transition={{delay: 0.5, duration: 1.2, ease: [0.56, 0.02, 0.37, 1.1]}}
-                >BE THE FIRST TO KNOW
-                </motion.h1>
-                </div>
-                    <motion.p 
-                        className='w-full md:w-[90%] pl-10 pr-10 lg:w-[70%] text-wrap mt-5 text-[14px] font-light'
-                        initial={{opacity:0}}
-                        animate={{opacity:1}}
-                        transition={{delay: 0.5, duration: 1.5, ease: [0.56, 0.02, 0.37, 1.1]}}
-                        >Would you like to be the first to hear about new exhibitions? 
+                <h1 className=' w-full lg:w-[70%] text-[72px] sm:text-[82px] text-black leading-none font-semibold pl-10 pr-10 lg:mr-20 lg:ml-20 text-left md:text-center'>BE THE FIRST TO KNOW</h1>
+                    </div>
+                        <p className='w-full md:w-[90%] pl-10 pr-10 lg:w-[70%] text-wrap mt-5 text-[14px] font-light'
+                        >
+                        Would you like to be the first to hear about new exhibitions? 
                         Then join the Gallery One email list! You will get updates on upcoming exhibitions and special 
                         offers, as well as exclusive access to early previews of all upcoming shows.
-                    </motion.p>
-
-                {/* change to input field and button*/}
-                <div className=' w-[100%] overflow-hidden'>
-                <motion.div 
-                    className='w-full flex flex-row justify-center items-end pr-10 pl-10'
-                    initial={{opacity:0}}
-                    animate={{opacity:1}}
-                    transition={{delay: 1.0, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                    >
-                        <input 
+                    </p>
+                <div className='w-full flex flex-row justify-center items-end pr-10 pl-10'>
+                    <input 
                         className='h-[40px] w-full pl-5 lg:w-[70%] border-2 border-black mt-20 flex flex-row justify-between items-center font-light text-gray-500 focus:outline-none'
                         placeholder='type your email'
-                        >
-                        </input>
-                            <button 
-                                type='button'
-                                className='h-[40px] w-[140px] flex flex-row justify-center items-center bg-black text-white text-[12px]'>
-                                SUBMIT    
-                            </button>
-            </motion.div>
+                    >
+                     </input>
+                    <button 
+                        type='button'
+                        className='h-[40px] w-[140px] flex flex-row justify-center items-center bg-black text-white text-[12px]'>
+                        SUBMIT    
+                    </button>
             </div>
         </div>
     </section>
