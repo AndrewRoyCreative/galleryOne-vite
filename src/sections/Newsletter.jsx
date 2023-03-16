5d63b3fcceeefa935346ad9f3b9f7448c4e9b014
AndrewRoyCreative <110363455+AndrewRoyCreative@users.noreply.github.com>
2023-03-16 14:23:33 -0400

    removed animations

--- src/sections/Newsletter.jsx
+++ src/sections/Newsletter.jsx
@@ -7,35 +7,17 @@ const Newsletter = () => {
     <section>
         <div className=' flex flex-col justify-start items-start md:justify-center md:items-center mt-40 lg:mt-64'>
             <div className='flex flex-row justify-center items-center overflow-hidden'>
-                <motion.h1 
-                    className=' w-full lg:w-[70%] text-[4rem] sm:text-[5.75rem] text-black leading-none font-semibold sm:pl-10 sm:pr-10 pr-5 pl-5 lg:mr-20 lg:ml-20 text-center sm:text-left'
-                    initial={{opacity:0}}
-                    whileInView={{opacity:1}}
-                    viewport={{ once: true }}
-                    transition={{delay: 0.9, duration: 1.2, ease: [0.56, 0.02, 0.37, 1.1]}}
-                    >
+                <h1 className=' w-full lg:w-[70%] text-[4rem] sm:text-[5.75rem] text-black leading-none font-semibold sm:pl-10 sm:pr-10 pr-5 pl-5 lg:mr-20 lg:ml-20 text-center sm:text-left'>
                     BE THE FIRST TO KNOW
-                    </motion.h1>
+                </h1>
             </div>
             <div className='w-full md:w-[80%] flex flex-col justify-center items-center md:text-center text-left'>
-                <motion.p 
-                    className='w-full md:w-[80%] pl-10 pr-10 text-wrap mt-5 text-[0.875rem] font-light text-center sm:text-left'
-                    initial={{opacity:0}}
-                    whileInView={{opacity:1}}
-                    viewport={{ once: true }}
-                    transition={{delay: 2.0, duration: 1.2, ease: [0.56, 0.02, 0.37, 1.1]}}
+                <p className='w-full md:w-[80%] pl-10 pr-10 text-wrap mt-5 text-[0.875rem] font-light text-center sm:text-left'
                     >Would you like to be the first to hear about new exhibitions? 
                     Then join the Gallery One email list! You will get updates on upcoming exhibitions and special 
                     offers, as well as exclusive access to early previews of all upcoming shows.
-                </motion.p>
-                <motion.div 
-                    className='w-full lg:w-[80%] flex flex-row justify-center items-end pr-10 pl-10'
-                    initial={{opacity:0}}
-                    animate={{opacity:1}}
-                    whileInView={{opacity:1}}
-                    viewport={{ once: true }}
-                    transition={{delay: 3.4, duration: 1.3, ease: [0.56, 0.02, 0.37, 1.1]}}
-                    >
+                </p>
+                <div className='w-full lg:w-[80%] flex flex-row justify-center items-end pr-10 pl-10'>
                     <input 
                         className='h-[2.5rem] w-full pl-5  border-2 border-black mt-20 flex flex-row justify-between items-center font-light text-gray-500 focus:outline-none rounded-none'
                         placeholder='type your email'
@@ -46,7 +28,7 @@ const Newsletter = () => {
                         >
                         SUBMIT    
                     </button>
-                </motion.div>
+                </div>
             </div>
         </div>
     </section>
